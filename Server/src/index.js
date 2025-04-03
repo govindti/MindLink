df0bbf18f39f5a67695684bd4ddcee59d2b30604
Govind Tiwari <108tiwari.g@gmail.com>
2025-04-03 13:05:54 +0530

    initialize server with Express, add basic routes and configuration files

---
+++ Server/src/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import jsonwebtoken from "jsonwebtoken";
+
+const app = express();
+
+app.post("api/v1/signup", (req, res) => {
+
+})
+
+app.post("api/v1/signin", (req, res) => {
+
+})
+
+app.post("api/v1/content", (req, res) => {
+
+})
+
+app.get("api/v1/content", (req, res) => {
+
+})
+
+app.delete("api/v1/content", (req, res) => {
+
+})
+
+app.post("api/v1/mindlink/share", (req, res) => {
+
+})
+
+app.get("api/v1/mindlink/:sharelink", (req, res) => {
+
+})
+
